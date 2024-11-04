81c4f81896f43290ee8b23949b861f9fc8b13ba8
andreferreiraext123 <mf1903491@gmail.com>
2024-11-04 11:22:27 -0300

    Bootstrap integration within two pages, and showing a list into a page

--- app/hello.py
+++ app/hello.py
@@ -5,13 +5,13 @@ app = Flask(__name__) # Initial instance of flask class
 @app.route('/') # Showing the route main (root route)
 @app.route('/homePage')
 def home_page():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/market')
 def market_page():
     items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '312312312', 'Price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '33333333', 'Price': 230},
-        {'id': 3, 'name': 'Headset', 'barcode': '32132111', 'Price': 100}
+        {'ID': 1, 'Name': 'Phone', 'Barcode': '312312312', 'Price': 500},
+        {'ID': 2, 'Name': 'Laptop', 'Barcode': '33333333', 'Price': 230},
+        {'ID': 3, 'Name': 'Headset', 'Barcode': '32132111', 'Price': 100}
     ]
     return render_template('market.html', items=items)
